9c09db5495c89575f485f8eaf50fc4bab11561de
Jake Cataford <Jake@Studio236.ca>
2013-05-11 14:31:55 -0400

    Changed indeces to be automatically generated :)

--- Assets/Rollout Core/Scripts/WaypointManager.cs
+++ Assets/Rollout Core/Scripts/WaypointManager.cs
@@ -17,44 +17,17 @@ public class WaypointManager : MonoBehaviour {
 			
 		}
 		
-		//Sanity check for indexes
-		
-		List<int> indeces = new List<int>();
-		List<GameObject> tempwaypoints = new List<GameObject>();
-		
-		
-		
-		foreach(GameObject waypoint in tempwaypoints) {
-			
-			//Check for dupes
-			
-			foreach(int index in indeces) {
-				if (waypoint.GetComponent<Waypoint>().index == index) {
-					Debug.LogError("Duplicate indeces in waypoints. see index: " + index);
-				}
-			}
-			
-			//add the current index
-			indeces.Add(waypoint.GetComponent<Waypoint>().index);
-			waypoints.Insert(waypoint.GetComponent<Waypoint>().index,waypoint);
+		//Assign unique indexes
+		int lastIndex = 0;
+		foreach(GameObject waypoint in tempWaypoints) {
+			waypoint.GetComponent<Waypoint>().index = lastIndex;
+			lastIndex++;
 		}
 		
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 	
 	void OnDrawGizmos() {
-		Gizmos.color = Color.yellow;
-		
-		
-		
-		for(int i = 0; i < waypoints.Count - 1; i++) {
-			Gizmos.DrawLine(waypoints[i].transform.position, waypoints[i + 1].transform.position);
-		}
 	}
 	
 	
