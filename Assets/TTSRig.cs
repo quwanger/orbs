cb6bdaa7209971012c35de0dd28ae8fd85892b85
Paden Shorey <paden@studio236.ca>
2013-07-14 18:54:34 -0400

    added and removed comments to script

--- Assets/TTSRig.cs
+++ Assets/TTSRig.cs
@@ -5,9 +5,10 @@ public class TTSRig : MonoBehaviour {
 
 	public float turnSpeed;
 	
-	// Update is called once per frame
 	void Update () {
-		//turnSpeed = transform.parent.parent.rigidbody.velocity.magnitude*50f;
+		
+		//this rig is nested under display mesh so that it keeps the same position
+		//below the velocity of the racer 2.0 is accessed in order to determine the rotation speed
 		turnSpeed = transform.parent.parent.rigidbody.velocity.magnitude*50f;
 		transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
 	}
