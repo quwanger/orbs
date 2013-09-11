4ca43b72cba55c0f5f2779a16ab819745ee0e2ea
Sunmock Yang <sunmock@live.ca>
2013-09-11 17:44:11 -0400

    Added documentation to Perishing behaviour, camera effects, and draw forward

--- Assets/Rollout Core/Scripts/Perishing Effects/TTSPerishingBehaviour.cs
+++ Assets/Rollout Core/Scripts/Perishing Effects/TTSPerishingBehaviour.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 using System.Collections;
 
+/*
+ * ============= TTSPerishingBehaviour, extends TTSBehaviour =================
+ * This class is used for any effect/object that will only last a set duration
+ * 
+ * Any class that extends this class will implement either/or
+ *  - OnPerishingUpdate(float progress)
+ *  - Kill()
+ *  
+ * You can use the public values to set how the update/kill functions will run
+ */
+
 public class TTSPerishingBehaviour : TTSBehaviour {
 	
-	public float duration = 5.0f;
-	public bool destroyWhenLifecycleComplete = true;
-	public bool useKillFunctionWhenComplete = false;
-	public bool useFixedUpdate = true;
+	public float duration = 5.0f; // Override this with your own values
+	public bool destroyWhenLifecycleComplete = true; // Once duration has passed, the class will stop running and self-destruct
+	public bool useKillFunctionWhenComplete = false; // Execute the kill function once complete
+	public bool useFixedUpdate = true; // Set this to true if you want to use FixedUpdate() instead of Update()
 	
 	private float birth;
 	private float _progress = 0.0f;
@@ -32,21 +43,20 @@ public class TTSPerishingBehaviour : TTSBehaviour {
 		_progress = Mathf.Lerp(0,1.0f,(Time.time - birth)/duration);
 		
 		OnPerishingUpdate(_progress);
-		
-		if((Time.time - birth)/duration > 1.0f) {
-			if(destroyWhenLifecycleComplete) {
-				Kill();
-				Destroy(this);
-			}
-			
-			if(useKillFunctionWhenComplete) {
-				Kill ();
-			}
+
+        if ((Time.time - birth) / duration > 1.0f) {
+            if (destroyWhenLifecycleComplete) {
+                Kill();
+                Destroy(this);
+            }
+            if (useKillFunctionWhenComplete)
+                Kill();
 		}
 	}
 	
 	//override this method to do your stuff.
 	protected virtual void OnPerishingUpdate(float progress) {
+        // Any code you want to run every frame
 	}
 	
 	
