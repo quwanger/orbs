40a1f653efe46939618476d2073cb9f8f6a21fad
Paden Shorey <paden@studio236.ca>
2013-09-18 11:49:01 -0400

    Cleaned up waypoint system.

--- Assets/Rollout Core/Scripts/TTSWaypoint.cs
+++ Assets/Rollout Core/Scripts/TTSWaypoint.cs
@@ -5,7 +5,6 @@ using System.Collections;
 public class TTSWaypoint : MonoBehaviour {
 	
 	/* Waypoint.cs
-	 * Jake Cataford
 	 * 
 	 * This class generates a waypoint and displays it properly.
 	 * The prefab of this object is flagged as a waypoint and the 
@@ -20,17 +19,14 @@ public class TTSWaypoint : MonoBehaviour {
 	public bool isActive = true;
 	private BoxCollider boxCollider;
 	
-	
 	void Start () {
 		boxCollider = GetComponent<BoxCollider>();
 		boxCollider.isTrigger = true;
 	}
 	
 	void OnDrawGizmos() {
-		
 		Gizmos.color = Color.black;
-		Gizmos.DrawIcon(this.transform.position,"waypoint-icon.png");
-		
+		Gizmos.DrawIcon(this.transform.position,"Rollout Core/Waypoints/waypoint-icon.png");
 	}
 	
 	void OnTriggerEnter(Collider other) {
