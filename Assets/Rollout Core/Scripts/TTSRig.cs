0cc9b15d1ddc956f387d15294c76f1f391426ce9
Paden Shorey <paden@studio236.ca>
2013-10-23 09:13:52 -0400

    Added scorpion rig

--- Assets/Rollout Core/Scripts/TTSRig.cs
+++ Assets/Rollout Core/Scripts/TTSRig.cs
@@ -17,7 +17,7 @@ public class TTSRig : MonoBehaviour {
 		
 		//this rig is nested under display mesh so that it keeps the same position
 		//below the velocity of the racer 2.0 is accessed in order to determine the rotation speed
-		rotationSpeed = transform.parent.parent.rigidbody.velocity.magnitude*50f;
-		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+		//rotationSpeed = transform.parent.parent.rigidbody.velocity.magnitude*50f;
+		//transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
 	}
 }
