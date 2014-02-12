993983abba24ae13df69557a9d34f6a439a098a3
Sunmock Yang <sunmock@live.ca>
2014-02-12 00:50:40 -0500

    Server code to sync objects together.

---
+++ Server/GoServer/src/UnityGo/UnityGo.go
@@ -0,0 +1,142 @@
+package UnityGo
+
+import (
+	"Packets"
+	"fmt"
+	"net"
+)
+
+// VECTOR3
+
+type Vector3 struct {
+	X float32
+	Y float32
+	Z float32
+}
+
+func (this *Vector3) Set(x float32, y float32, z float32) {
+	this.X = x
+	this.Y = y
+	this.Z = z
+}
+
+func (this *Vector3) Add(add *Vector3) *Vector3 {
+	a := new(Vector3)
+	a.X, a.Y, a.Z = this.X+add.X, this.Y+add.Y, this.Z+add.Z
+	return a
+}
+
+// UNITY COMMAND
+
+type UnityCommand struct {
+	Address   *net.UDPAddr
+	OutPacket *Packets.PacketWriter
+
+	OutConn *net.UDPConn
+
+	VAR_SIZE int
+}
+
+func (this *UnityCommand) Init(writer *Packets.PacketWriter, address *net.UDPAddr, outConn *net.UDPConn) {
+	this.OutPacket = writer
+	this.Address = address
+	this.OutConn = outConn
+	this.VAR_SIZE = 4
+}
+
+func (this *UnityCommand) checkWritable(dataLength int) {
+	if this.OutPacket.Size()+dataLength > len(this.OutPacket.Data) {
+		fmt.Printf(">	POTENTIAL DATA OVERFLOW AT %v BYTES. SENDING/CLEARING PACKET\n", this.OutPacket.Size())
+		this.SendPacket()
+		this.OutPacket.Clear()
+	}
+}
+
+func (this *UnityCommand) ReplyInit() {
+	this.checkWritable(this.VAR_SIZE)
+	this.OutPacket.WriteUInt32(2)
+}
+
+func (this *UnityCommand) ObjectUpdate(data []byte) {
+	this.checkWritable(len(data))
+	this.OutPacket.WriteBytes(data)
+}
+
+func (this *UnityCommand) ObjectNotRegistered(id float32) {
+	this.checkWritable(this.VAR_SIZE * 2)
+	this.OutPacket.WriteUInt32(5)
+	this.OutPacket.WriteFloat32(id)
+}
+
+func (this *UnityCommand) ObjectAlreadyRegistered(id float32) {
+	this.checkWritable(this.VAR_SIZE * 2)
+	fmt.Printf("*	OBJECT #%v REGISTERED TO DIFFERENT OWNER\n\n", id)
+	this.OutPacket.WriteUInt32(6)
+	this.OutPacket.WriteFloat32(id)
+}
+
+func (this *UnityCommand) ObjectRegisteredSuccess(id float32) {
+	this.checkWritable(this.VAR_SIZE * 2)
+	fmt.Printf("X	Added new obj %v\n\n", id)
+	this.OutPacket.WriteUInt32(11)
+	this.OutPacket.WriteFloat32(id)
+}
+
+func (this *UnityCommand) ClearData() {
+	this.OutPacket.Clear()
+}
+
+func (this *UnityCommand) SendPacket() {
+	if this.OutPacket.Size() == 0 {
+		return
+	}
+
+	n, err := this.OutConn.WriteToUDP(this.OutPacket.Data[0:this.OutPacket.Size()], this.Address)
+	fmt.Printf("<-- Sending %v bytes to %v\n\n", n, this.Address)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// UNITY CONNECTION
+
+type UnityConn struct {
+	Address       *net.UDPAddr
+	ReturnAddress *net.UDPAddr
+	IsEstablished bool
+	OwnedObjects  []float32
+	OutPacket     *Packets.PacketWriter
+	CommandSender *UnityCommand
+}
+
+func (this *UnityConn) Init(sender *net.UDPAddr, outPacket *Packets.PacketWriter, outConn *net.UDPConn) {
+	this.Address = sender
+	this.ReturnAddress = &net.UDPAddr{IP: sender.IP, Port: 6969}
+	this.IsEstablished = false
+	this.OutPacket = outPacket
+	this.CommandSender = new(UnityCommand)
+	this.CommandSender.Init(this.OutPacket, this.ReturnAddress, outConn)
+}
+
+func (this *UnityConn) AddObj(id float32) {
+	this.OwnedObjects = append(this.OwnedObjects, id)
+}
+
+// UNITY GAMEOBJECT
+
+type UnityGameObject struct {
+	Id        float32
+	LastPos   *Vector3
+	LastRot   *Vector3
+	LastSpeed *Vector3
+	Owner     *net.UDPAddr
+}
+
+func (this *UnityGameObject) Init(id float32, owner *net.UDPAddr) {
+	this.Id = id
+	this.LastPos = new(Vector3)
+	this.LastRot = new(Vector3)
+	this.LastSpeed = new(Vector3)
+	this.Owner = owner
+}
---
+++ Server/GoServer/src/main.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"Packets"
+	"UnityGo"
+	"errors"
+	"fmt"
+	"net"
+	"time"
+)
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Constants
+const INIT_CONNECTION_TIMEOUT = 5000
+
+// Connections/Objects
+var connections map[string]*UnityGo.UnityConn // Mapped using the IP string
+var objs map[float32]*UnityGo.UnityGameObject
+var inConn, outConn *net.UDPConn
+
+func InitServer() {
+	println("")
+	println("  ▒█████   ██▀███   ▄▄▄▄     ██████ ")
+	println(" ▒██▒  ██▒▓██ ▒ ██▒▓█████▄ ▒██    ▒ ")
+	println(" ▒██░  ██▒▓██ ░▄█ ▒▒██▒ ▄██░ ▓██▄   ")
+	println(" ▒██   ██░▒██▀▀█▄  ▒██░█▀    ▒   ██▒")
+	println(" ░ ████▓▒░░██▓ ▒██▒░▓█  ▀█▓▒██████▒▒")
+	println(" ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░░▒▓███▀▒▒ ▒▓▒ ▒ ░")
+	println("   ░ ▒ ▒░   ░▒ ░ ▒░▒░▒   ░ ░ ░▒  ░ ░")
+	println(" ░ ░ ░ ▒    ░░   ░  ░    ░ ░  ░  ░  ")
+	println("     ░ ░     ░      ░            ░  ")
+	println("                         ░          ")
+	println("                                2014")
+	println("____________________________________")
+
+	connections = make(map[string]*UnityGo.UnityConn)
+	objs = make(map[float32]*UnityGo.UnityGameObject)
+
+	var err error
+
+	inConn, err = net.ListenUDP("udp", &net.UDPAddr{net.IPv4zero, 6666, ""})
+	chk(err)
+
+	outConn, err = net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: 0})
+	chk(err)
+}
+
+func main() {
+	InitServer()
+
+	for {
+		packetReader := new(Packets.PacketReader)
+		packetReader.Data = make([]byte, 1024)
+		_, address, err := inConn.ReadFromUDP(packetReader.Data)
+
+		// println(n)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		go ProcessPacket(address, packetReader)
+	}
+}
+
+func ProcessPacket(sender *net.UDPAddr, reader *Packets.PacketReader) {
+	var command int = int(reader.ReadUInt32())
+
+	var outPacket *Packets.PacketWriter
+
+	if _, keyExists := connections[sender.IP.String()]; keyExists {
+		outPacket = connections[sender.IP.String()].OutPacket
+	} else {
+		outPacket = new(Packets.PacketWriter)
+		outPacket.InitPacket()
+	}
+
+	for command != 0 {
+
+		fmt.Printf("Received command: '%v' from %v\n", command, sender)
+
+		switch command {
+
+		// First Connection
+		case 1:
+			// Establish connection
+			connection, keyExists := connections[sender.IP.String()]
+
+			if !keyExists {
+				fmt.Printf("X	STARTING CONNECTION TO: %v\n\n", sender.IP)
+				connection = new(UnityGo.UnityConn)
+				connection.Init(sender, outPacket, outConn)
+				connections[sender.IP.String()] = connection
+			}
+
+			ReturnFire(connection)
+
+			go TimeoutConnection(sender.IP.String())
+
+		// Handshake complete
+		case 3:
+			connections[sender.IP.String()].IsEstablished = true
+			fmt.Printf("X	Connection to %v established\n\n", sender.IP.String())
+
+		// UnityGo.Unity object Update
+		case 4:
+			ObjectUpdate(reader, connections[sender.IP.String()], outPacket)
+
+		// UnityGo.Unity Object Registration
+		case 10:
+			RegisterObject(reader, connections[sender.IP.String()], outPacket)
+
+		// Close connection
+		case 9999:
+			CloseConnection(sender.IP.String())
+		}
+
+		command = reader.ReadInt32()
+	}
+
+	for _, value := range connections {
+		value.CommandSender.SendPacket()
+		value.CommandSender.ClearData()
+	}
+
+	/*
+		SendPacket(outPacket.Data[0:outPacket.Size()], returnAddress)
+		outPacket.Clear()
+
+		var pos = new(UnityGo.UnityGo.Vector3)
+		pos.X, pos.Y, pos.Z = reader.ReadFloat32(), reader.ReadFloat32(), reader.ReadFloat32()
+
+		fmt.Printf("%v: %v, %v\n", command, pos, sender)
+	*/
+}
+
+// case 1: reply with 2
+func ReturnFire(connection *UnityGo.UnityConn) {
+	connection.CommandSender.ReplyInit()
+}
+
+// case 4:	Save position, broadcast to other clients.
+// 			Reply 5 if obj not registered, 6 if someone else registered
+func ObjectUpdate(reader *Packets.PacketReader, connection *UnityGo.UnityConn, outPacket *Packets.PacketWriter) error {
+	var id = reader.ReadFloat32()
+	obj, keyExists := objs[id]
+
+	if !keyExists {
+		connection.CommandSender.ObjectNotRegistered(id)
+
+		reader.EmptyRead12Bytes()
+		reader.EmptyRead12Bytes()
+		reader.EmptyRead12Bytes()
+
+		return errors.New("OBJECT NOT REGISTERED")
+	} else if obj.Owner != connection.Address {
+		connection.CommandSender.ObjectAlreadyRegistered(id)
+
+		reader.EmptyRead12Bytes()
+		reader.EmptyRead12Bytes()
+		reader.EmptyRead12Bytes()
+
+		return errors.New("OBJECT REGISTERED TO DIFFERENT OWNER")
+	}
+
+	obj.LastPos.Set(reader.ReadFloat32(), reader.ReadFloat32(), reader.ReadFloat32())
+	obj.LastRot.Set(reader.ReadFloat32(), reader.ReadFloat32(), reader.ReadFloat32())
+	obj.LastSpeed.Set(reader.ReadFloat32(), reader.ReadFloat32(), reader.ReadFloat32())
+
+	fmt.Printf("*	Update Obj: '%v' currently at %v\n\n", id, obj.LastPos)
+
+	for _, value := range connections {
+		value.CommandSender.ObjectUpdate(ObjectUpdateToBytes(obj))
+	}
+
+	return nil
+}
+
+func ObjectUpdateToBytes(obj *UnityGo.UnityGameObject) []byte {
+	var broadcastPacket = new(Packets.PacketWriter)
+	broadcastPacket.InitPacket()
+	broadcastPacket.WriteUInt32(4)
+	broadcastPacket.WriteFloat32(obj.Id)
+
+	broadcastPacket.WriteVector3(obj.LastPos.X, obj.LastPos.Y, obj.LastPos.Z)
+	broadcastPacket.WriteVector3(obj.LastRot.X, obj.LastRot.Y, obj.LastRot.Z)
+	broadcastPacket.WriteVector3(obj.LastSpeed.X, obj.LastSpeed.Y, obj.LastSpeed.Z)
+
+	return broadcastPacket.GetMinimalData()
+}
+
+// case 10:	reply with 11 to acknowledge, 6 if already registered
+func RegisterObject(reader *Packets.PacketReader, connection *UnityGo.UnityConn, outPacket *Packets.PacketWriter) {
+	var id = reader.ReadFloat32()
+	_, keyExists := objs[id]
+
+	if keyExists {
+		connection.CommandSender.ObjectAlreadyRegistered(id)
+	} else {
+		objs[id] = new(UnityGo.UnityGameObject)
+		objs[id].Init(id, connection.Address)
+
+		connection.AddObj(id)
+
+		connection.CommandSender.ObjectRegisteredSuccess(id)
+	}
+}
+
+func CloseConnection(IpAddress string) {
+	if _, ok := connections[IpAddress]; !ok {
+		return
+	}
+
+	fmt.Printf("!	%v has shut down connection\n\n", IpAddress)
+
+	for _, value := range connections[IpAddress].OwnedObjects {
+		delete(objs, value)
+	}
+
+	delete(connections, IpAddress)
+}
+
+func TimeoutConnection(ipKey string) {
+	time.Sleep(INIT_CONNECTION_TIMEOUT * time.Millisecond)
+
+	if _, exists := connections[ipKey]; exists && !connections[ipKey].IsEstablished {
+		fmt.Printf("Initial connection from %v has timed out\n\n", ipKey)
+		delete(connections, ipKey)
+	}
+}
