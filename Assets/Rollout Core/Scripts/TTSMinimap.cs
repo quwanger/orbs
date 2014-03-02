c4c7a6fe8bd585a820c08613404c23824852db4a
Quang Nguyen <quwanger@Quangs-MacBook-Pro.local>
2014-03-02 11:26:42 -0500

    Minimap tweaks

--- Assets/Rollout Core/Scripts/TTSMinimap.cs
+++ Assets/Rollout Core/Scripts/TTSMinimap.cs
@@ -7,9 +7,14 @@ public class TTSMinimap : MonoBehaviour {
 	public RenderTexture minimapTexture;
 	public Material minimapMaterial;
 
+	private int numPlayers;
+	private int numRacers;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameObject levelGO = GameObject.Find("TTSLevel");
+		numPlayers = levelGO.GetComponent<TTSInitRace>().tempNumHumanPlayers;
+		numRacers = levelGO.GetComponent<TTSInitRace>().numberOfRacers;
 	}
 	
 	// Update is called once per frame
@@ -19,9 +24,42 @@ public class TTSMinimap : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		if(Event.current.type == EventType.Repaint) {
-			Graphics.DrawTexture(new Rect(20, Screen.height - 220, 200, 200), minimapTexture, minimapMaterial);
-			Graphics.DrawTexture(new Rect(150, 150, 200, 200), minimapTexture, minimapMaterial);
+		if(Event.current.type == EventType.Repaint)	{
+			for(int i = 0; i < numRacers; i++) {
+				switch(numPlayers){
+					case 2:
+						if(i==0) {
+							Graphics.DrawTexture(new Rect(10, (Screen.height / 2) - 110, 100, 100), minimapTexture, minimapMaterial);
+						} else {
+							Graphics.DrawTexture(new Rect(10, (Screen.height - 110) , 100, 100), minimapTexture, minimapMaterial);
+						}
+						break;
+
+					case 3:
+						if(i==0)
+							Graphics.DrawTexture(new Rect(10, (Screen.height / 2) - 110, 100, 100), minimapTexture, minimapMaterial);
+						else if(i==1)
+							Graphics.DrawTexture(new Rect(10, Screen.height - 110, 100, 100), minimapTexture, minimapMaterial);
+						else
+							Graphics.DrawTexture(new Rect((Screen.width / 2) + 10, Screen.height - 110, 100, 100), minimapTexture, minimapMaterial);
+						break;
+
+					case 4:
+						if(i==0)
+							Graphics.DrawTexture(new Rect(10, (Screen.height / 2) - 110, 100, 100), minimapTexture, minimapMaterial);
+						else if(i==1)
+							Graphics.DrawTexture(new Rect((Screen.width / 2) + 10, (Screen.height / 2) - 110, 100, 100), minimapTexture, minimapMaterial);
+						else if(i==2)
+							Graphics.DrawTexture(new Rect(10, Screen.height - 110, 100, 100), minimapTexture, minimapMaterial);
+						else
+							Graphics.DrawTexture(new Rect((Screen.width / 2) + 10, Screen.height - 110, 100, 100), minimapTexture, minimapMaterial);
+						break;
+
+					default:
+						Graphics.DrawTexture(new Rect(10, Screen.height - 210, 200, 200), minimapTexture, minimapMaterial);
+					break;
+				}
+			}
 		}
 	}
 }
