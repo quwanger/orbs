cc3535fa10b6578dd3f10cac4dce893b8e61c34d
Paden Shorey <paden@studio236.ca>
2014-03-04 13:53:49 -0500

    Hud animations complete, indicators now work properly

--- Assets/Rollout Core/Scripts/TTSHudPowerup.cs
+++ Assets/Rollout Core/Scripts/TTSHudPowerup.cs
@@ -61,21 +61,21 @@ public class TTSHudPowerup : MonoBehaviour {
 		}
 		
 		if(t!=3){
-			iTween.ScaleTo(powerupFrame,iTween.Hash("scale", new Vector3 (powerupFrame.transform.localScale.x*frameScaleFactor,powerupFrame.transform.localScale.y*frameScaleFactor,powerupFrame.transform.localScale.z*frameScaleFactor) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
-			iTween.ScaleTo(powerupLevel,iTween.Hash("scale", new Vector3 (powerupLevel.transform.localScale.x*frameScaleFactor,powerupLevel.transform.localScale.y*frameScaleFactor,powerupLevel.transform.localScale.z*frameScaleFactor) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
-			iTween.ScaleTo(this.gameObject,iTween.Hash("scale", new Vector3 (this.gameObject.transform.localScale.x*frameScaleFactor,this.gameObject.transform.localScale.y*frameScaleFactor,this.gameObject.transform.localScale.z*frameScaleFactor) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
+			iTween.ScaleTo(powerupFrame,iTween.Hash("scale", new Vector3 (0.3f, 0.3f, 0.3f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
+			iTween.ScaleTo(powerupLevel,iTween.Hash("scale", new Vector3 (0.3f, 0.3f, 0.09231377f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
+			iTween.ScaleTo(this.gameObject,iTween.Hash("scale", new Vector3 (0.3f, 0.3f, 0.3f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
 			
-			powerupFrame.renderer.material.color = Color.white;
-			powerupLevel.renderer.material.color = Color.white;
-			this.gameObject.renderer.material.color = Color.white;
+			//powerupFrame.renderer.material.color = Color.white;
+			//powerupLevel.renderer.material.color = Color.white;
+			//this.gameObject.renderer.material.color = Color.white;
 		}else{
-			iTween.ScaleTo(powerupFrame,iTween.Hash("scale", new Vector3 (powerupFrame.transform.localScale.x*frameScaleFactor*1.5f,powerupFrame.transform.localScale.y*frameScaleFactor*1.5f,powerupFrame.transform.localScale.z*frameScaleFactor*1.5f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
-			iTween.ScaleTo(powerupLevel,iTween.Hash("scale", new Vector3 (powerupLevel.transform.localScale.x*frameScaleFactor*1.5f,powerupLevel.transform.localScale.y*frameScaleFactor*1.5f,powerupLevel.transform.localScale.z*frameScaleFactor*1.5f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
-			iTween.ScaleTo(this.gameObject,iTween.Hash("scale", new Vector3 (this.gameObject.transform.localScale.x*frameScaleFactor*1.5f,this.gameObject.transform.localScale.y*frameScaleFactor*1.5f,this.gameObject.transform.localScale.z*frameScaleFactor*1.5f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
+			iTween.ScaleTo(powerupFrame,iTween.Hash("scale", new Vector3 (0.4f, 0.4f, 0.4f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
+			iTween.ScaleTo(powerupLevel,iTween.Hash("scale", new Vector3 (0.4f, 0.4f, 0.121854f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
+			iTween.ScaleTo(this.gameObject,iTween.Hash("scale", new Vector3 (0.4f, 0.4f, 0.4f) ,"time", 0.1f, "oncomplete", "ReturnToOriginalSize", "oncompletetarget", this.gameObject));
 			
-			powerupFrame.renderer.material.color = Color.cyan;
-			powerupLevel.renderer.material.color = Color.cyan;
-			this.gameObject.renderer.material.color = Color.cyan;
+			//powerupFrame.renderer.material.color = Color.cyan;
+			//powerupLevel.renderer.material.color = Color.cyan;
+			//this.gameObject.renderer.material.color = Color.cyan;
 		}
 		
 		hpl.UpdatePowerupLevel(t);
--- Assets/TTSIndicator.cs
+++ Assets/TTSIndicator.cs
@@ -9,9 +9,15 @@ public class TTSIndicator : MonoBehaviour {
 	
 	public Camera myCamera;
 	
+	public float myScale = 0.1f;
+	
+	float ratioW = Screen.width;
+	float ratioH = Screen.height;
+	float ratio;
+	
 	// Use this for initialization
 	void Start () {
-	
+		ratio =  ratioW/ratioH;
 	}
 	
 	// Update is called once per frame
@@ -20,11 +26,29 @@ public class TTSIndicator : MonoBehaviour {
 			screenPosition = myCamera.WorldToScreenPoint(racerToIndicate.transform.position);
 			
 			if(screenPosition.z > 0){
-				Rect newInset = new Rect(screenPosition.x, screenPosition.y, screenPosition.z, screenPosition.z);
 				
-				float scaleFactor = screenPosition.z/100.0f;
+				ratioW = Screen.width;
+				ratioH = Screen.height;
+				ratio = ratioW/ratioH;
 				
-				this.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+				//Debug.Log(screenPosition.z);
+				
+				float scaleFactor = screenPosition.z;
+				
+				Rect newInset = new Rect(screenPosition.x, screenPosition.y, 1f, 1f);
+				
+				if(scaleFactor<1.0f)
+					scaleFactor = 1.0f;
+				else if(scaleFactor > 300.0f)
+					scaleFactor = 300.0f;
+				
+				//this is to increase the size to make it visible
+				scaleFactor /= 100.0f;
+				
+				Vector3 newPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+				
+				this.transform.position = newPosition;
+				this.transform.localScale = new Vector3((myScale/ratio)/scaleFactor, myScale/scaleFactor, 1f);
 				
 				this.GetComponent<GUITexture>().pixelInset = newInset;
 			}
